7bcd539a07deba78bcecc9a05025442bbae58c98
Chris Bannister <c.bannister@gmail.com>
2009-02-23 11:38:40 +0000

    ITs a core

---
+++ core.lua
@@ -0,0 +1,8 @@
+local fux = LibStub("AceAddon-3.0"):NewAddon("Fux")
+local quixote = LibStub("LibQuixote-2.0")
+
+function fux:OnEnable()
+	self.Zones = {}
+	self.ZonesByName = {}
+	self.zoneCount = 0
+end
