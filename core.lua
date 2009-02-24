ed401a1a66e005cd9748fca0f37997cb6733f644
Chris Bannister <c.bannister@gmail.com>
2009-02-24 18:29:23 +0000

    Move frame into core

--- core.lua
+++ core.lua
@@ -1,12 +1,38 @@
 local fux = LibStub("AceAddon-3.0"):NewAddon("Fux")
 local Q = LibStub("LibQuixote-2.0")
 
+function fux:OnInitialize()
+	local f = CreateFrame("Frame", nil, UIParent)
+	f:SetHeight(300)
+	f:SetWidth(200)
+	f:SetBackdrop({
+		bgFile = "Interface\\ChatFrame\\ChatFrameBackground", tile = true, tileSize = 16,
+		edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border", edgeSize = 10,
+		insets = {left = 1, right = 1, top = 1, bottom = 1},
+	})
+	f:SetBackdropColor(0, 0, 0, 0.8)
+
+	f:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 0, -500)
+	f:Show()
+
+	local t = f:CreateFontString(nil, "OVERLAY")
+	t:SetPoint("TOP", f, "TOP", 0, -3)
+	t:SetPoint("LEFT")
+	t:SetPoint("RIGHT")
+	t:SetFont(STANDARD_TEXT_FONT, 16)
+	t:SetText("Fux Title")
+
+	f.title = t
+
+	self.frame = f
+end
+
 function fux:OnEnable()
 	self.zones = {}
 	self.ZonesByName = {}
 	self.zoneCount = 0
 
-	Q.RegisterCallback(self, "Quixote_Update", "QuestUpdate")
+	Q.RegisterCallback(self, "Update", "QuestUpdate")
 	self:QuestUpdate()
 end
 
